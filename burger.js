557f95ffe8c6a5817478917951045902c3dfb348
Zofux <70540447+Zofux@users.noreply.github.com>
2025-06-02 00:27:06 +0200

    Update

--- burger.js
+++ burger.js
@@ -18,3 +18,24 @@ burger.addEventListener('click', () => {
 
     links.classList.toggle("open")
 });
+
+const mobilLinks = document.querySelectorAll(".mobil-link")
+mobilLinks.forEach(link => {
+    link.addEventListener("click", () => {
+
+        if (burger.classList.contains('rotate')) {
+            // Gå tilbake til start
+            burger.classList.remove('rotate');
+            setTimeout(() => {
+                burger.classList.remove('squish');
+            }, 300); // matcher CSS-transition
+        } else {
+            // Start animasjonen
+            burger.classList.add('squish');
+            setTimeout(() => {
+                burger.classList.add('rotate');
+            }, 150); // delay før rotasjon (kan justeres)
+        }
+        links.classList.toggle("open")
+    })
+})
